8bf2fa53efcb2678a04271df9037a89625f6f832
danenespoli <danenespoli@gmail.com>
2018-01-10 13:41:22 -0500

    Add working image URL and sale prices to Jcrew

--- app/client/stores/JCrewStore.js
+++ app/client/stores/JCrewStore.js
@@ -8,9 +8,9 @@ const JcrewStore = {
       try {
         return _.map((body.productList[0].products || []), (item) => ({
           name: item.productDescription,
-          imgSm: `https://www.jcrew.com/s7-img-facade/${item.productCode}_${item.defaultColorCode}_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&wid=408&hei=408`,
-          price: parseFloat(item.listPrice.amount),
-          sale: false,
+          imgSm: `https://www.jcrew.com/s7-img-facade/${item.baseProductCode ? item.baseProductCode : item.productCode}_${item.defaultColorCode}?fmt=jpeg&resMode=sharp`,
+          price: parseFloat(item.now ? item.now.amount : item.listPrice.amount),
+          sale: item.now,
           link: `https://jcrew.com${item.url}`,
           logo: 'img/jcrew.png',
           logoHeight: '20px',
